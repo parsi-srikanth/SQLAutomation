8fa9ad38761d42e313e96af786ccdb42f61e12dc
Srikanth Parsi <parsi.srikanth218@gmail.com>
2024-03-05 16:49:08 -0600

    updates

--- database_helper.py
+++ database_helper.py
@@ -8,8 +8,10 @@ logger = logging.getLogger(__name__)
 def connect_to_database(connection_string):
     try:
         con = cx_Oracle.connect(connection_string)
+        logger.info("Connected to database")
         return con
     except Exception as e:
+        logger.error(f"Error: {e}")
         return None
 
 def execute_sql_queries(directory, parameters, connection_string):
@@ -22,15 +24,16 @@ def execute_sql_queries(directory, parameters, connection_string):
                     with open(file_path, 'r') as f:
                         sql_query = f.read()
                         for key, value in parameters.items():
-                            # logger.info(f"Replacing {key} with {value}")
                             sql_query = sql_query.replace(key, value)
                         logger.info(f"SQL query after parameter replacement: {sql_query}")
                         con = connect_to_database(connection_string)
                         if con:
                             cursor = con.cursor()
                             cursor.execute(sql_query)
+                            logger.info("execute query")
                             columns = [col[0] for col in cursor.description]
                             data = cursor.fetchall()
+                            logger.info("fetched results")
                             result = pd.DataFrame(data, columns=columns)
                             result.to_csv(os.path.join(folder_path, f"{os.path.splitext(file)[0]}.csv"), index=False)
                             logger.info(f"csv created in {folder_path}")
@@ -38,3 +41,4 @@ def execute_sql_queries(directory, parameters, connection_string):
                             con.close()
                 except Exception as e:
                     print(e)
+                    logger.error(f"Error: {e}") 
